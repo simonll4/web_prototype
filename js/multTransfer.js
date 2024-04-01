e5a8a3c1c1b8aa8a96c845045766a4271c79bb99
simonll4 <sllamosas806@alumnos.iua.edu.ar>
2024-04-01 02:22:04 -0300

    navegacion de trasnferencia multiple(sin validacion)

--- js/multTransfer.js
+++ js/multTransfer.js
@@ -1,32 +1,32 @@
 // Obtiene todas las secciones
-const sections = document.querySelectorAll("#main-m section");
+const multipleTransferSections = document.querySelectorAll("#main-m section");
 let currentSectionIndex = 0;
 
 // etapas de progress bar
 const steps = document.querySelectorAll("#progressbar li");
 
 document.getElementById("next-btn-2").addEventListener("click", () => {
-  if (currentSectionIndex < sections.length - 1) {
+  if (currentSectionIndex < multipleTransferSections.length - 1) {
     currentSectionIndex++;
   }
 
-  if (currentSectionIndex < sections.length) {
+  if (currentSectionIndex < multipleTransferSections.length) {
     // Oculta sección actual
-    sections[currentSectionIndex - 1].style.display = "none";
+    multipleTransferSections[currentSectionIndex - 1].style.display = "none";
 
     // progress bar
-    if (sections[currentSectionIndex].id === "section2") {
+    if (multipleTransferSections[currentSectionIndex].id === "section2") {
       steps[0].classList.remove("active");
       document.querySelector("#progressbar #first").classList.add("completed");
       steps[1].classList.add("active");
 
-      if (sections[currentSectionIndex].id === "section2") {
+      if (multipleTransferSections[currentSectionIndex].id === "section2") {
         document.getElementById("title").innerText = "Verificacion";
       }
     }
 
     // Muestra la sección actual
-    sections[currentSectionIndex].style.display = "block";
+    multipleTransferSections[currentSectionIndex].style.display = "block";
   }
 });
 
@@ -34,7 +34,7 @@ document.getElementById("back-btn-2").addEventListener("click", () => {
   if (currentSectionIndex > 0) {
     currentSectionIndex--;
 
-    if (sections[currentSectionIndex].id === "section1") {
+    if (multipleTransferSections[currentSectionIndex].id === "section1") {
       steps[1].classList.remove("active");
       document
         .querySelector("#progressbar #first")
@@ -43,8 +43,8 @@ document.getElementById("back-btn-2").addEventListener("click", () => {
     }
 
     // Oculta sección actual
-    sections[currentSectionIndex + 1].style.display = "none";
+    multipleTransferSections[currentSectionIndex + 1].style.display = "none";
 
-    sections[currentSectionIndex].style.display = "block";
+    multipleTransferSections[currentSectionIndex].style.display = "block";
   }
 });
--- js/transfer.js
+++ js/transfer.js
@@ -1,117 +1,206 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  btnNext = document.getElementById("nextbtn");
+  nextBtn = document.getElementById("nextbtn");
+  let selectedRadio = null;
 
-  btnNext.addEventListener("click", () => {
-    let selectedRadio = localStorage.getItem("selectedRadio");
+  // activar boton next
+  document.addEventListener("iframechange", () => {
+    selectedRadio = localStorage.getItem("selectedRadio");
+    if (selectedRadio) {
+      nextBtn.disabled = false;
+    }
+  });
+
+  nextBtn.addEventListener("click", () => {
+    selectedRadio = localStorage.getItem("selectedRadio");
     if (selectedRadio === "singletransfer") {
       singleTransferNext();
     }
     if (selectedRadio === "multipletransfer") {
+      multipleTransferNext();
     }
   });
 
+  document.addEventListener("backbtn", () => {
+    if (selectedRadio === "singletransfer") {
+      singleTransferback();
+    }
+    if (selectedRadio === "multipletransfer") {
+      multipleTransferback();
+    }
+  });
+
+  /////////////////////////////////////////////////////////////////////////////////
+  /////////////// TRANSFERENCIA UNICA /////////////////////////////////////////////
+  //secciones single transfer
+  const singleTransferSections = document.querySelectorAll(
+    "section[id^='st'], #transferstartsect, #transferreceipsect, #validatetransfersect"
+  );
   // indice de las secciones
   let currentStSectionIndex = 0;
 
   function singleTransferNext() {
-    if (currentStSectionIndex < singleTranferSections.length - 1) {
+    if (currentStSectionIndex < singleTransferSections.length - 1) {
       currentStSectionIndex++;
     }
 
-    if (currentStSectionIndex < singleTranferSections.length - 1) {
+    if (currentStSectionIndex < singleTransferSections.length - 1) {
       // Oculta sección actual
-      singleTranferSections[currentStSectionIndex - 1].style.display = "none";
+      singleTransferSections[currentStSectionIndex - 1].style.display = "none";
 
       // activar progress bar
-      if (singleTranferSections[currentStSectionIndex].id === "st1sect") {
+      if (singleTransferSections[currentStSectionIndex].id === "st1sect") {
         document.querySelector("#progressbarsect").style.display = "block";
       }
 
       // cambio de estado de la progress bar
       if (
-        (singleTranferSections[currentStSectionIndex].id === "st2sect") |
-        (singleTranferSections[currentStSectionIndex].id === "st4sect")
+        (singleTransferSections[currentStSectionIndex].id === "st2sect") |
+        (singleTransferSections[currentStSectionIndex].id === "st4sect")
       ) {
         //dispara evento para capturarlo en progressBar.js
         window.dispatchEvent(new CustomEvent("nextbuttonclicked"));
       }
 
       // cambio de titulo
-      if (singleTranferSections[currentStSectionIndex].id === "st3sect") {
+      if (singleTransferSections[currentStSectionIndex].id === "st3sect") {
         document.getElementById("title").innerText = "Verificacion";
       }
 
       // Oculta boton next
       if (
-        singleTranferSections[currentStSectionIndex].id ===
+        singleTransferSections[currentStSectionIndex].id ===
         "validatetransfersect"
       ) {
-        btnNext.style.display = "none";
+        nextBtn.style.display = "none";
       }
 
       // Muestra la sección actual
-      singleTranferSections[currentStSectionIndex].style.display = "block";
+      singleTransferSections[currentStSectionIndex].style.display = "block";
     }
   }
-
-  // activar boton next
-  document.addEventListener("iframechange", () => {
-    let selectedRadio = localStorage.getItem("selectedRadio");
-    if (selectedRadio) {
-      btnNext.disabled = false;
-    }
-  });
 
   document.addEventListener("verifiedidentity", () => {
     currentStSectionIndex++;
     // ocultar progress bar
     if (
-      singleTranferSections[currentStSectionIndex].id === "transferreceipsect"
+      singleTransferSections[currentStSectionIndex].id === "transferreceipsect"
     ) {
       document.querySelector("#progressbarsect").style.display = "none";
       // Oculta sección actual
-      singleTranferSections[currentStSectionIndex - 1].style.display = "none";
+      singleTransferSections[currentStSectionIndex - 1].style.display = "none";
       // Muestra la sección actual
-      singleTranferSections[currentStSectionIndex].style.display = "block";
+      singleTransferSections[currentStSectionIndex].style.display = "block";
     }
   });
 
-  //secciones single transfer
-  const singleTranferSections = document.querySelectorAll(
-    "section[id^='st'], #transferstartsect, #transferreceipsect, #validatetransfersect"
-  );
-
-  document.addEventListener("backbtn", () => {
+  function singleTransferback() {
     if (currentStSectionIndex > 0) {
       currentStSectionIndex--;
 
       if (
-        (singleTranferSections[currentStSectionIndex].id === "st1sect") |
-        (singleTranferSections[currentStSectionIndex].id === "st3sect")
+        (singleTransferSections[currentStSectionIndex].id === "st1sect") |
+        (singleTransferSections[currentStSectionIndex].id === "st3sect")
       ) {
         window.dispatchEvent(new CustomEvent("backbuttonclicked"));
       }
-      if (singleTranferSections[currentStSectionIndex].id === "st4sect") {
-        btnNext.style.display = "block";
+      if (singleTransferSections[currentStSectionIndex].id === "st4sect") {
+        nextBtn.style.display = "block";
       }
 
       if (
-        singleTranferSections[currentStSectionIndex].id ===
+        singleTransferSections[currentStSectionIndex].id ===
         "validatetransfersect"
       ) {
         document.querySelector("#progressbarsect").style.display = "block";
       }
 
       if (
-        singleTranferSections[currentStSectionIndex].id === "transferstartsect"
+        singleTransferSections[currentStSectionIndex].id === "transferstartsect"
       ) {
         document.querySelector("#progressbarsect").style.display = "none";
       }
 
       // Oculta sección actual
-      singleTranferSections[currentStSectionIndex + 1].style.display = "none";
+      singleTransferSections[currentStSectionIndex + 1].style.display = "none";
       // muestra sección anterior
-      singleTranferSections[currentStSectionIndex].style.display = "block";
+      singleTransferSections[currentStSectionIndex].style.display = "block";
     }
-  });
+  }
+
+  /////////////////////////////////////////////////////////////////////////////////
+  /////////////// TRANSFERENCIA MULTIPLE //////////////////////////////////////////
+  //secciones multiple transfer
+  const multipleTransferSections = document.querySelectorAll(
+    "section[id^='mt'], #transferstartsect, #transferreceipsect, #validatetransfersect"
+  );
+  let currentSectionIndex = 0;
+
+  function multipleTransferNext() {
+    if (currentSectionIndex < multipleTransferSections.length - 1) {
+      currentSectionIndex++;
+    }
+
+    if (currentSectionIndex < multipleTransferSections.length) {
+      // Oculta sección actual
+      multipleTransferSections[currentSectionIndex - 1].style.display = "none";
+      // Muestra la sección actual
+      multipleTransferSections[currentSectionIndex].style.display = "block";
+
+      // activar progress bar
+      if (multipleTransferSections[currentSectionIndex].id === "mt1sect") {
+        document.querySelector("#progressbarsect").style.display = "block";
+      }
+
+      // cambio de titulo
+      if (multipleTransferSections[currentSectionIndex].id === "mt2sect") {
+        document.getElementById("title").innerText = "Verificacion";
+      }
+
+      if (
+        (multipleTransferSections[currentSectionIndex].id === "mt2sect") |
+        (multipleTransferSections[currentSectionIndex].id ===
+          "validatetransfersect")
+      ) {
+        window.parent.dispatchEvent(new CustomEvent("nextbuttonclicked"));
+      }
+
+      // Oculta boton next
+      if (
+        multipleTransferSections[currentSectionIndex].id ===
+        "validatetransfersect"
+      ) {
+        nextBtn.style.display = "none";
+      }
+    }
+  }
+
+  function multipleTransferback() {
+    if (currentSectionIndex > 0) {
+      currentSectionIndex--;
+
+      if (
+        (multipleTransferSections[currentSectionIndex].id === "mt1sect") |
+        (multipleTransferSections[currentSectionIndex].id ===
+          "mt2sect")
+      ) {
+        window.dispatchEvent(new CustomEvent("backbuttonclicked"));
+      }
+      //desactivar progress bar
+      if (
+        multipleTransferSections[currentSectionIndex].id === "transferstartsect"
+      ) {
+        document.querySelector("#progressbarsect").style.display = "none";
+      }
+      // activar boton
+      if (multipleTransferSections[currentSectionIndex].id === "mt2sect") {
+        nextBtn.style.display = "block";
+      }
+
+
+      // Oculta sección actual
+      multipleTransferSections[currentSectionIndex + 1].style.display = "none";
+
+      multipleTransferSections[currentSectionIndex].style.display = "block";
+    }
+  }
 });
